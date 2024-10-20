5ab844789660ef1e6a5c2d094c1da8e2ba3a2e85
2iedo <162278702+2iedo@users.noreply.github.com>
2024-10-20 16:39:53 +0900

    feat : Product 클래스 추가, 메소드로 유사도 점수 변경

--- product_recommand_algorithm/score_weight.py
+++ product_recommand_algorithm/score_weight.py
@@ -1,44 +1,43 @@
 from datetime import datetime
 
-# 평균 별점에 따른 가중치 계산, product_list는 (productName, averageStarCount, review, postDate, score) 순서로 데이터 받음(튜플 형태)
-def addStarPointScore(product_list):
-    for product in product_list:
-        averageStarCount = product[1]
-        score = product[4]
-        if averageStarCount < 1:
-            product[4] = score * 0.9
-        elif averageStarCount < 2:
-            product[4] = score * 0.95
-        elif averageStarCount < 3:
-            product[4] = score
-        elif averageStarCount < 4:
-            product[4] = score * 1.05
-        else:
-            product[4] = score * 1.1
+class Product:
+    def __init__(self, productName, averageStarCount, review, postDate):
+        self.productName = productName
+        self.averageStarCount = averageStarCount
+        self.review = review
+        self.postDate = postDate
+        self.score = 0
 
-# 리뷰수에 따른 가중치 계산, product_list는 (productName, averageStarCount, review, postDate, score) 순서로 데이터 받음(튜플 형태)
-def addReviewCountScore(product_list):
-    for product in product_list:
-        review = product[2]
-        score = product[4]
-        if review <= 100:
-            product[4] = score
-        elif review <= 1000:
-            product[4] = score * 1.05
+    # 평균 별점에 따른 가중치 계산
+    def addStarPointScore(self):
+        if self.averageStarCount < 1:
+            self.score = self.score * 0.9
+        elif self.averageStarCount < 2:
+            self.score = self.score * 0.95
+        elif self.averageStarCount < 3:
+            self.score = self.score
+        elif self.averageStarCount < 4:
+            self.score = self.score * 1.05
         else:
-            product[4] = score * 1.1
+            self.score = self.score * 1.1
 
-# 제품 등록일에 따른 가중치 계산, product_list는 (productName, averageStarCount, review, postDate, score) 순서로 데이터 받음(튜플 형태)
-def addPostDateScore(product_list):
-    for product in product_list:
-        postDate = product[3]
-        score = product[4]
+    # 리뷰수에 따른 가중치 계산
+    def addReviewCountScore(self):
+        if self.review <= 100:
+            self.score = self.score
+        elif self.review <= 1000:
+            self.score = self.score * 1.05
+        else:
+            self.score = self.score * 1.1
+
+    # 제품 등록일에 따른 가중치 계산
+    def addPostDateScore(self):
         today = datetime.now().date()
+        daysDifference = (today - self.postDate).days
 
-        daysDifference = (today - postDate).days
         if daysDifference < 7:
-            product[4] = score * 1.15
+            self.score = self.score * 1.15
         elif daysDifference < 30:
-            product[4] = score * 1.1
-        elif daysDifference < 180:
-            product[4] = score * 1.05
+            self.score = self.score * 1.1
+        elif daysDifference < 100:
+            self.score = self.score * 1.05
